7c1690c8e8aebc5036550fe4e678066792f9fbc3
Krishna Bhargav Reddy Chilukuri <kreddy@ucsd.edu>
2022-05-01 05:11:59 -0700

    Update 1

---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(10, SkillDemo.mul( 3, 3));
+    }
+ 
+}
